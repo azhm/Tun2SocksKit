515e3d7513322d4e3e66e05a2469450e0e0f6b59
azhm <azhminfo@gmail.com>
2023-06-13 09:11:01 +0000

    Tun2SocksKit Release 10.15_test_1.0!!!

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
     ),
     .binaryTarget(
         name: "HevSocks5Tunnel",
-        url: "https://github.com/arror/Tun2SocksKit/releases/download/2.4.0/HevSocks5Tunnel.xcframework.zip",
-        checksum: "3f0f72711b02b0f1ed363c1b5760ff0f94e9b13851f5f95658b63314e6517e5d"
+        url: "https://github.com/arror/Tun2SocksKit/releases/download/10.15_test_1.0/HevSocks5Tunnel.xcframework.zip",
+        checksum: "774dae6f4cf0cb0f20865ab6c43a439e79b00ebfbe04dc89fa94c59692cedca5"
     )
   ]
 )
